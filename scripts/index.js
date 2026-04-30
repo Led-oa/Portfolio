291ab1e0c65f4cdb98965a04ba14183b7433822c
Led-oa <ledoagael@gmail.com>
2026-04-30 10:38:02 +0300

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+const username = "Led-oa"; // ← remplace
+
+fetch(`https://api.github.com/users/${username}/repos`)
+  .then((res) => res.json())
+  .then((data) => {
+    const container = document.getElementById("projects");
+
+    // Filtrer (optionnel)
+    const repos = data
+      .filter((repo) => !repo.fork) // enlève les forks
+      .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+
+    repos.forEach((repo) => {
+      const card = document.createElement("div");
+      card.classList.add("project_card");
+
+      card.innerHTML = `
+                <h5 class="project_name">${repo.name}</h5>
+                <p>${repo.description || "No description"}</p>
+                <p>🧠 ${repo.language || "Unknown"}</p>
+                <p>⭐ ${repo.stargazers_count}</p>
+                <a href="${repo.html_url}" target="_blank">Voir</a>
+            `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch((err) => console.error(err));
+
+
+  const navLinks = document.querySelectorAll('.nav_link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const linkText = this.textContent.trim();
+        let sectionId = '';
+        
+        if (linkText === 'Who I Am') sectionId = 'whoiam';
+        else if (linkText === 'My Skills') sectionId = 'myskills';
+        else if (linkText === 'My Projects') sectionId = 'myprojects';
+        else if (linkText === 'Contacts') sectionId = 'contacts';
+        
+        const targetSection = document.getElementById(sectionId);
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
